db796cf453ba33b4b801fd8e9907f0060f1ef391
VarGun <heegun46900@gmail.com>
2025-08-04 13:36:09 +0900

    initial commit

---
+++ src/services/api.ts
@@ -0,0 +1,38 @@
+import axios from "axios";
+import type {Product} from "../types/Product.ts";
+
+const api = axios.create({
+  // .env 파일로 이동 필요, 배포시 별도 관리 필요
+  baseURL: 'http://localhost:8080/api',
+  headers: {
+    'Content-Type': 'application/json',
+  },
+});
+
+api.interceptors.request.use(
+    config => {
+      const token = localStorage.getItem('token');
+      if (token) {
+        config.headers['Authorization'] = `Bearer ${token}`;
+      }
+      return config;
+    },
+    error => Promise.reject(error)
+);
+
+export const productService = {
+  getAll: (): Promise<Product[]> =>
+      api.get('/products').then(res => res.data),
+
+  getById: (id: number): Promise<Product> =>
+      api.get(`/products/${id}`).then(res => res.data),
+
+  create: (data: Omit<Product, 'id'>): Promise<Product> =>
+      api.post('/products', data).then(res => res.data),
+
+  update: (id: number, data: Partial<Omit<Product, 'id'>>): Promise<Product> =>
+      api.put(`/products/${id}`, data).then(res => res.data),
+
+  delete: (id: number): Promise<void> =>
+      api.delete(`/products/${id}`).then(res => res.data),
+};
